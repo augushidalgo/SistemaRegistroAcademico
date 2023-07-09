fd78cde42829d802220cd0f0f540f0019e087806
augushidalgo <53703995+augushidalgo@users.noreply.github.com>
2023-07-09 17:20:55 -0600

    Modificado el nombre del botón reportes

--- SistemaRegistroAcademico/Form1.Designer.cs
+++ SistemaRegistroAcademico/Form1.Designer.cs
@@ -154,7 +154,7 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(317, 99);
-            this.button1.Name = "button1";
+            this.button1.Name = "btnreportes";
             this.button1.Size = new System.Drawing.Size(136, 115);
             this.button1.TabIndex = 8;
             this.button1.Text = "Reportes";
